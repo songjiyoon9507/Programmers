e57c783c8bdbb022666f73aa3cc80b43968521aa
songjiyoon9507 <156267654+songjiyoon9507@users.noreply.github.com>
2024-02-03 23:59:07 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -4,7 +4,6 @@ function solution(my_string, overwrite_string, s) {
     // 빈 문자열에 my_string 넣고
     // 문자열을 배열로 반환 split 함수 사용
     str = my_string.split("");
-    // console.log(str);
     // str 배열에서 splice 메서드 사용
     // splice 배열에서 기존 요소 삭제, 교체, 새 요소 추가
     // splice(배열 시작점, 삭제 요소의 수, 배열에 추가할 요소,...)추가할 요소는 뒤에 ,로 쭉 나열할 수 있음
