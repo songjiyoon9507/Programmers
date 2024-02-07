799f997c596b4cd86881442bc7cd5b7a046f95fb
songjiyoon9507 <156267654+songjiyoon9507@users.noreply.github.com>
2024-02-08 01:03:23 +0900

    [Bronze IV] Title: 영수증, Time: 224 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		int i = N;
+		int sum = 0;
+		for(i = 1 ; i <= N ; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			sum += a*b;
+		}
+		if (X == sum) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
