6b558e5783d15fa014fe4327d831f9fbbc604ee2
songjiyoon9507 <156267654+songjiyoon9507@users.noreply.github.com>
2024-02-18 20:35:44 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.nextLine().charAt(0);
+		
+		System.out.println((int)ch);
+	}
+}
