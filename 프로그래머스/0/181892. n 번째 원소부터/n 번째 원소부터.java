22b56d6e3862e45913b6ac27db4df4cf15a1ba8a
JiYun Song <156267654+songjiyoon9507@users.noreply.github.com>
2024-08-28 16:26:56 +0900

    [level 0] Title: n 번째 원소부터, Time: 3.07 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = n-1 ; i < num_list.length ; i++) {
+            list.add(num_list[i]);
+        }
+        
+        return list.stream().mapToInt(x->x).toArray();
+    }
+}
