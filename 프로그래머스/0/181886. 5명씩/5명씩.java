26d0b3767e6becb3114c337a6fb8604cd29b5232
JiYun Song <156267654+songjiyoon9507@users.noreply.github.com>
2024-09-06 15:23:15 +0900

    [level 0] Title: 5명씩, Time: 9.75 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+        
+        List<String> list = new ArrayList<>();
+        
+        for(int i = 0 ; i < names.length ; i++) {
+            if(i % 5 == 0) {
+                list.add(names[i]);
+            }
+        }
+        
+        return list.stream().toArray(String[]::new);
+    }
+}
