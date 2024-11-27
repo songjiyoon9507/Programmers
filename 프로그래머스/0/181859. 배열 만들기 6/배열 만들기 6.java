eddb1e3a2d50d690bd113e896cd15ff3b126d8a4
JiYun Song <156267654+songjiyoon9507@users.noreply.github.com>
2024-11-27 14:01:17 +0900

    [level 0] Title: 배열 만들기 6, Time: 58.02 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        
+        int i = 0;
+        
+        while( i < arr.length ) {
+            if(stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) == arr[i]) {
+                stk.remove(stk.size() - 1);
+                i++;
+            } else {
+                stk.add(arr[i]);
+                i++;
+            }
+        }
+        
+        if(stk.isEmpty()) {
+            stk.add(-1);
+        }
+        
+        return stk.stream().mapToInt(x->x).toArray();
+    }
+}
