6c421728ff18bc56dea9b7c1dcd5b5c0985849cc
JiYun Song <156267654+songjiyoon9507@users.noreply.github.com>
2025-04-01 21:49:02 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.58 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length - 5];
+        Arrays.sort(num_list);
+        int j = 0;
+        for(int i = 5 ; i < num_list.length ; i++) {
+            answer[j] = num_list[i];
+            j++;
+        }
+        return answer;
+    }
+}
