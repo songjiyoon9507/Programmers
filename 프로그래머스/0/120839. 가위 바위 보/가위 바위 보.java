822cf0c175e081173c93e084c174d8795fc76bfc
JiYun Song <156267654+songjiyoon9507@users.noreply.github.com>
2025-11-30 08:23:53 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : rsp.toCharArray()) {
+            switch(c) {
+                case '0' :
+                    sb.append("5");
+                    break;
+                case '2' :
+                    sb.append("0");
+                    break;
+                case '5' :
+                    sb.append("2");
+                    break;
+                default : break;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
